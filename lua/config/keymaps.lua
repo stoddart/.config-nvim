780f7092f5d5baeec698a50056a4c2c9064bc2cb
Daniel Stoddart <stoddart@gmail.com>
2025-02-28 17:15:53 +0800

    Wrote separate keymaps file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,30 @@
 -- Add any additional keymaps here
 --
 vim.cmd("let g:netrw_liststyle = 3")
+
+-- Key mappings using LazyVim's approach
+local function map(mode, lhs, rhs, opts)
+  opts = opts or {}
+  opts.silent = opts.silent ~= false
+  if opts.remap and opts.noremap == nil then
+    opts.noremap = false
+  end
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+-- Set up new mappings
+map("n", "gcc", "<cmd>Commentary<CR>", { desc = "Comment line" })
+map("n", "yy", "<Plug>(YankyYank)", { desc = "Yank line with Yanky" })
+map("n", "oo", "o<Esc>", { desc = "Insert line below" })
+map("n", "Y", "<Plug>(YankyYank)", { desc = "Yank line with Yanky" })
+
+-- Register which-key mappings
+local wk = require("which-key")
+wk.register({
+  ["gcc"] = { name = "Comment line", mode = { "n" } },
+  ["yy"] = { name = "Yank line with Yanky", mode = { "n" } },
+  ["oo"] = { name = "Insert line below", mode = { "n" } },
+  ["Y"] = { name = "Yank line with Yanky", mode = { "n" } },
+  ["F"] = { name = "Around word", mode = { "n" } },
+  ["w"] = { name = "Inside word", mode = { "n" } },
+})
