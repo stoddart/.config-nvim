35e25a32b3d9d4c88915b022c552ee6418bb5cd2
Daniel Stoddart <stoddart@gmail.com>
2025-02-28 17:15:14 +0800

    Reduced to only the bootstrap code and basic setup

--- init.lua
+++ init.lua
@@ -12,67 +12,11 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Setup lazy.nvim
-require("lazy").setup({
-  spec = {
-    { import = "plugins" },
-  },
-  defaults = {
-    lazy = true, -- all plugins are lazy-loaded by default
-    version = false, -- always use the latest git commit
-  },
-  install = { colorscheme = { "tokyonight", "habamax" } },
-  checker = { enabled = true }, -- automatically check for plugin updates
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "gzip",
-        "tarPlugin",
-        "tohtml",
-        "tutor",
-        "zipPlugin",
-      },
-    },
-  },
-})
+-- Set Python 3 host program for Neovim
+vim.g.python3_host_prog = "/Users/daniel/.pyenv/shims/python3"
 
--- Bootstrap lazy.nvim, LazyVim, and plugins
+-- Load LazyVim and plugins
 require("config.lazy")
 
 -- Set options
 vim.o.startofline = true
-
--- Set Python 3 host program for Neovim
-vim.g.python3_host_prog = "/Users/daniel/.pyenv/shims/python3"
-
--- Key mappings using LazyVim's approach
-local function map(mode, lhs, rhs, opts)
-  opts = opts or {}
-  opts.silent = opts.silent ~= false
-  if opts.remap and opts.noremap == nil then
-    opts.noremap = false
-  end
-  vim.keymap.set(mode, lhs, rhs, opts)
-end
-
--- Remove conflicting keymaps
-vim.keymap.del("n", "gc")
-vim.keymap.del("n", "y")
-vim.keymap.del("n", "o")
-
--- Set up new mappings
-map("n", "gcc", "<cmd>Commentary<CR>", { desc = "Comment line" })
-map("n", "yy", "<Plug>(YankyYank)", { desc = "Yank line with Yanky" })
-map("n", "oo", "o<Esc>", { desc = "Insert line below" })
-map("n", "Y", "<Plug>(YankyYank)", { desc = "Yank line with Yanky" })
-
--- Register which-key mappings
-local wk = require("which-key")
-wk.register({
-  ["gcc"] = { name = "Comment line", mode = { "n" } },
-  ["yy"] = { name = "Yank line with Yanky", mode = { "n" } },
-  ["oo"] = { name = "Insert line below", mode = { "n" } },
-  ["Y"] = { name = "Yank line with Yanky", mode = { "n" } },
-  ["F"] = { name = "Around word", mode = { "n" } },
-  ["w"] = { name = "Inside word", mode = { "n" } },
-})
