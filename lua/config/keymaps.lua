dc766e93e09d061c6d46ef4c03bb28b5d07899db
Daniel Stoddart <stoddart@gmail.com>
2025-02-28 17:26:14 +0800

    Removed explicit which-key setup since LazyVim already handles this

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -21,39 +21,3 @@ map("n", "oo", "o<Esc>", { desc = "Insert line below" })
 map("n", "Y", "<Plug>(YankyYank)", { desc = "Yank line with Yanky" })
 map("n", "F", "", { desc = "Around word" })
 map("n", "w", "", { desc = "Inside word" })
-
--- Set up which-key groups
-require("which-key").setup({
-  plugins = {
-    marks = true,
-    registers = true,
-    spelling = {
-      enabled = true,
-      suggestions = 20,
-    },
-  },
-  operators = { gc = "Comments" },
-  key_labels = {},
-  icons = {
-    breadcrumb = "»",
-    separator = "➜",
-    group = "+",
-  },
-  popup_mappings = {
-    scroll_down = "<c-d>",
-    scroll_up = "<c-u>",
-  },
-  window = {
-    border = "single",
-    position = "bottom",
-    margin = { 1, 0, 1, 0 },
-    padding = { 1, 2, 1, 2 },
-    winblend = 0,
-  },
-  layout = {
-    height = { min = 4, max = 25 },
-    width = { min = 20, max = 50 },
-    spacing = 3,
-    align = "left",
-  },
-})
