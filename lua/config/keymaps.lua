c3c3f1ab207517ec23c76b28833bedaa5b333a7f
Daniel Stoddart <stoddart@gmail.com>
2025-02-28 17:20:04 +0800

    Removed the deprecated wk.register calls

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,19 +14,46 @@ local function map(mode, lhs, rhs, opts)
   vim.keymap.set(mode, lhs, rhs, opts)
 end
 
--- Set up new mappings
+-- Set up new mappings with which-key integration
 map("n", "gcc", "<cmd>Commentary<CR>", { desc = "Comment line" })
 map("n", "yy", "<Plug>(YankyYank)", { desc = "Yank line with Yanky" })
 map("n", "oo", "o<Esc>", { desc = "Insert line below" })
 map("n", "Y", "<Plug>(YankyYank)", { desc = "Yank line with Yanky" })
+map("n", "F", "", { desc = "Around word" })
+map("n", "w", "", { desc = "Inside word" })
 
--- Register which-key mappings
-local wk = require("which-key")
-wk.register({
-  ["gcc"] = { name = "Comment line", mode = { "n" } },
-  ["yy"] = { name = "Yank line with Yanky", mode = { "n" } },
-  ["oo"] = { name = "Insert line below", mode = { "n" } },
-  ["Y"] = { name = "Yank line with Yanky", mode = { "n" } },
-  ["F"] = { name = "Around word", mode = { "n" } },
-  ["w"] = { name = "Inside word", mode = { "n" } },
+-- Set up which-key groups
+require("which-key").setup({
+  plugins = {
+    marks = true,
+    registers = true,
+    spelling = {
+      enabled = true,
+      suggestions = 20,
+    },
+  },
+  operators = { gc = "Comments" },
+  key_labels = {},
+  icons = {
+    breadcrumb = "»",
+    separator = "➜",
+    group = "+",
+  },
+  popup_mappings = {
+    scroll_down = "<c-d>",
+    scroll_up = "<c-u>",
+  },
+  window = {
+    border = "single",
+    position = "bottom",
+    margin = { 1, 0, 1, 0 },
+    padding = { 1, 2, 1, 2 },
+    winblend = 0,
+  },
+  layout = {
+    height = { min = 4, max = 25 },
+    width = { min = 20, max = 50 },
+    spacing = 3,
+    align = "left",
+  },
 })
