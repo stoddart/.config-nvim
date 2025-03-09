f70a85e5f1871ffa2bc42d8ca2d6f7a86b6cbfa5
Daniel Stoddart <stoddart@gmail.com>
2025-03-09 15:58:13 +0800

    Update lockfile

---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,51 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup({
+        size = 20,
+        open_mapping = [[<c-\>]],
+        hide_numbers = true,
+        shade_filetypes = {},
+        shade_terminals = true,
+        shading_factor = 2,
+        start_in_insert = true,
+        insert_mappings = true,
+        persist_size = true,
+        direction = "float",
+        close_on_exit = true,
+        shell = vim.o.shell,
+        float_opts = {
+          border = "curved",
+          winblend = 0,
+          highlights = {
+            border = "Normal",
+            background = "Normal",
+          },
+        },
+      })
+
+      -- Custom terminal functions
+      local Terminal = require("toggleterm.terminal").Terminal
+
+      -- Lazygit terminal
+      local lazygit = Terminal:new({
+        cmd = "lazygit",
+        hidden = true,
+        direction = "float",
+      })
+
+      -- Function to toggle lazygit
+      function _lazygit_toggle()
+        lazygit:toggle()
+      end
+
+      -- Key mappings
+      vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+      vim.api.nvim_set_keymap("n", "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", {noremap = true, silent = true})
+      vim.api.nvim_set_keymap("n", "<leader>th", "<cmd>ToggleTerm size=10 direction=horizontal<cr>", {noremap = true, silent = true})
+      vim.api.nvim_set_keymap("n", "<leader>tv", "<cmd>ToggleTerm size=80 direction=vertical<cr>", {noremap = true, silent = true})
+    end,
+  },
+} 
